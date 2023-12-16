93e365f634bc2cc0346f5aa45ec9664486bac5f0
seangwoong <rje287573@gmail.com>
2023-12-17 01:57:26 +0900

    :sparkles: :Feat HookSetting

---
+++ src/hooks/Gpt/useGptChatOptions.jsx
@@ -0,0 +1,104 @@
+import { useQuery ,useMutation} from '@tanstack/react-query';
+import axios from 'axios';
+
+/**
+ * GPT 채팅 기능을 처리하는 커스텀 훅입니다.
+ * 데이터 가져오기 및 관리를 위해 react-query를 사용합니다.
+ * 60초마다 재검색 간격 설정 (60 * 1000 밀리초)
+ * @returns {Object} Data 값이나 mtate 함수
+ */
+export default function useGptChatOptions() {
+  
+  /**
+   * 서버에서 제품을 가져옵니다.
+   * 
+   * @returns {Promise} 가져온 데이터로 해결되는 프로미스입니다.
+   * @throws {Error} 요청이 실패하거나 응답 상태가 200이 아닌 경우 발생합니다.
+   */
+  const GetGptOption = async () => {
+    try {
+      const response = await axios.get('http://localhost:8003/chat/alladmin/');
+
+      if (response.status !== 200) {
+        throw new Error('제품을 가져오는 데 실패했습니다');
+      }
+
+      const data = response.data;
+   
+
+      return data;
+    } catch (error) {
+      console.error('제품을 가져오는 중 오류 발생:', error);
+      throw error;
+    }
+  };
+
+
+  /**
+   * 서버에 제품을 전송합니다.
+   * 
+   * @param {Object} params - 포스트 요청의 url입니다.
+   * @param {string} params.id - 포스트 요청의 ID입니다.
+   * @param {string} params.Prompt - 포스트 요청의 프롬프트입니다.
+   * @param {string} params.Response - 포스트 요청의 응답입니다.
+   * 
+   * @returns {Promise} 포스트 요청의 데이터로 해결되는 프로미스입니다.
+   * @throws {Error} 포스트 요청이 실패한 경우 발생합니다.
+   */
+  const PutGptOption = async ({ id, Prompt, Response }) => {
+    try {
+      
+      const response = await axios.put(`http://127.0.0.1:8003/chat/alladmin/${id}/`, {
+        'prompt': Prompt,
+        'response': Response,
+      });
+  
+ 
+      
+
+      const data = response.data;
+     
+
+      return data;
+    } catch (error) {
+      if (error.response) {
+        // 서버가 응답을 반환한 경우
+        console.error('Server responded with:', error.response.data);
+        console.error('Status code:', error.response.status);
+        console.error('Headers:', error.response.headers);
+      } else {
+        // 서버에 요청을 보내기 전에 오류가 발생한 경우
+        console.error('Error message:', error.message);
+      }
+      
+
+
+      console.error('제품 전송 중 오류 발생:', error);
+      throw error;
+    }
+  };
+
+  const { mutate: mutatePutGptOption } = useMutation({
+    mutationFn: PutGptOption,
+    onSuccess: () => {
+      // Handle success
+      gptRefetch();
+    },
+  });
+  
+
+
+
+ 
+
+  
+  const  { data: gptData, error: gptError, refetch:gptRefetch } = useQuery({
+    queryKey: ['GptRool'],
+    queryFn: GetGptOption,
+    refetchInterval:false, //
+  });
+
+
+
+  return { gptData, mutatePutGptOption };
+}
---
+++ src/hooks/useWindowSzie.jsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react';
+
+/**
+ * 윈도우 너비에 기반하여 현재 화면 크기를 결정하는 훅입니다.
+ *
+ * @property {Object} 현재 화면 크기를 담고 있는 객체입니다.
+ * @returns {string} screenSize - 현재 화면 크기 에따라서 ('sm', 'md', 'lg', 'xl','xxl')값이 나옵니다.
+ */
+const useWindowSize = () => {
+
+  
+
+
+  /**
+   * 윈도우 너비를 기반으로 화면 크기를 계산하는 함수입니다.
+   *
+   * @param {number} width - 윈도우 너비입니다.
+   * @returns {string} 계산된 화면 크기입니다.
+   */
+  const getScreenSize = (width) => {
+
+
+    // 각 화면 크기에 대한 최대 너비를 정의하는 배열입니다.
+   
+    const breakpoints = [640, 768, 1024, 1280];
+    let size = "none"
+    switch (true) {
+      case width <= breakpoints[0]:
+        size = 'sm';
+        break;
+      case width <= breakpoints[1]:
+        size = 'md';
+        break;
+      case width <= breakpoints[2]:
+        size = 'lg';
+        break;
+      case width <= breakpoints[3]:
+        size = 'xl';
+        break;
+      default:
+        break;
+    }
+    console.log(size)
+    return size;
+  };
+
+  const [screenSize, setScreenSize] = useState(getScreenSize(window.innerWidth));
+
+  useEffect(() => {
+    /**
+     * 윈도우 리사이즈 이벤트를 처리하는 핸들러입니다.
+     */
+    const handleResize = () => {
+      setScreenSize(getScreenSize(window.innerWidth));
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []); // 최초 마운트 시에만 실행되는 효과
+
+  return { screenSize };
+};
+
+
+export default useWindowSize;
