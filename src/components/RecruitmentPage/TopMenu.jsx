9ae397883b3b944d9cc3a966c26c1e75db118269
seangwoong <rje287573@gmail.com>
2023-12-22 08:16:55 +0900

    :bug: 	Fix: TopMenuAPI

--- src/components/RecruitmentPage/TopMenu.jsx
+++ src/components/RecruitmentPage/TopMenu.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react';
 import Contour from '../ui/Contour'
 import Margin from '../Margin'
+import { AuthContext } from '../../context/AuthContext';
 
 function TopMenu() {
+  const [profileData, setProfileData] = useState({
+    profilePicture: '',
+    nickname: '',
+    email: '',
+    profile_image:'',
+  });
+
+  const {getUserInfo} = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getUserInfo();
+        setProfileData({
+          profile_image: data.profile_image || '',
+          nickname: data.nickname || '',
+          email: data.email || '',
+        });
+
+      } catch (error) {
+        console.error('Failed to fetch user profile:', error);
+      }
+    };
+
+    fetchData();
+
+  }, []);
+
+
   return (
     <div>  
     <div className='w-72 h-full flex justify-center items-center'>
@@ -12,45 +42,35 @@ function TopMenu() {
 
 
       {/* 프로필 정보 */}
-    
-
-        <div className='flex  self-start mx-3 my-8'>
-        <div className='relative overflow-hidden rounded-ful  rounded-full bg-black h-6 w-6  '>
-          <img
-            src='프로필사진의_이미지_경로.jpg'
-            alt='프로필 사진'
-            className='h-7 w-7 rounded-full'
-          />
-     
-          
-
-       
-  
+        <div className='flex self-start mx-3 my-4'>
+          <div className='flex items-center'>
+            <div className='relative overflow-hidden rounded-full bg-black h-12 w-12'>
+              <img src={profileData.profile_image}
+                alt='프로필 사진'
+                className='h-16 w-16 rounded-full'/>
+            </div>
+            <Margin left="3"></Margin>
+            {/* 사진과 높이가 맞도록 */}
+            <div className='flex items-center px-4'>
+              {profileData.nickname}
+            </div>
+          </div>
         </div>
-        <Margin left="3"></Margin>
-        <div className=''>유저 A</div>
+<Contour></Contour>
+<div className='m-3 flex justify-around'>
+      {/* 게시물 */}
+      <div className='rounded border border-gray-400 bg-gray-200
+      hover:bg-gray-300 text-gray-700 hover:text-gray-800
+        justify-center align-middle min-w-max p-2'>
+        내 게시물
       </div>
 
-
-<Contour></Contour>
-
-<div className='m-3 flex  justify-around'>
-      {/* 버튼 */}
-      <div className='font-mono rounded   w-20 h-8 border border-gray-400 bg-gray-200
-       hover:bg-gray-300 text-gray-700 hover:text-gray-800
-        justify-center align-middle
-       '
-        >
-          게시물 0
-        </div>
-
-      <div className='font-mono rounded w-20 h-8 border border-gray-400 bg-gray-200
-       hover:bg-gray-300 text-gray-700 hover:text-gray-800
-       justify-center align-middle
-       '
-        >
-          댓글 0
-        </div>
+      {/* 댓글 버튼  */}
+      <div className='rounded border border-gray-400 bg-gray-200
+      hover:bg-gray-300 text-gray-700 hover:text-gray-800
+      justify-center align-middle min-w-max p-2'>
+        내 댓글
+      </div>
     </div>
     </div>
   </div></div>
