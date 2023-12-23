b6df0d33b45da4835204444ea4ea1176cf3cde6e
dwanGim <nugoory20@gmail.com>
2023-12-23 20:27:13 +0900

    :bug: Fix : Left menu에 유저정보 요청 및 기입

--- src/components/LeftMenu.jsx
+++ src/components/LeftMenu.jsx
@@ -1,39 +1,43 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import Margin from './Margin'
 import DynamicColorButton from './DynamicColorButton'
 import Contour from './ui/Contour'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../context/AuthContext.jsx';
 
 function LeftMenu() {
-  const [userDataList, setUserDataList] = useState([]);
-  useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const user = JSON.parse(localStorage.getItem("user"));
-        const accessToken = user?.access_token || "";
-        if (!accessToken) {
-          console.error("Access token not available");
-          return null;
-        }
-        const response = await axios.get(
-          "http://127.0.0.1:8000/users/4/profile/",
-          {
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-            },
-          }
-        );
-        setUserDataList(response.data);
+  // 사용되는 페이지 = /users/myteams/, /recruits/
+  
 
-        return response.data;
+  const { getUserInfo } = useContext(AuthContext);
+  const [profileData, setProfileData] = useState({
+    profilePicture: '',
+    nickname: '',
+    profile_content: '',
+    profile_image:'',
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getUserInfo();
+        setProfileData({
+          profile_image: data.profile_image || '/media/default_image.png',
+          nickname: data.nickname || '',
+          email: data.email || '',
+          profile_content: data.profile_content || "안녕하세요.",
+        });
+
       } catch (error) {
-        console.error("Fetching data failed:", error.message);
-        return null;
+        console.error('Failed to fetch user profile:', error);
       }
     };
-    fetchUserData();
-  }, []);
+
+    fetchData();
+  } ,[]);
+
+
   return (
     <div className='w-72 h-full flex justify-center items-center'>
     <div className='mt-4 w-64 h-full mx-4 border-[1px]  rounded-md bg-gray-100 flex flex-col items-center text-center'>
@@ -45,12 +49,16 @@ function LeftMenu() {
       <div>
         <div className='relative overflow-hidden rounded-full bg-black h-28 w-28'>
           <img
-            src={userDataList.profile_image}
+            src={profileData.profile_image}
             alt='프로필 사진'
             className='프로필_이미지_스타일 rounded-full'
           />
         </div>
         <Margin top="5"   plustailwind="h-5" />
+        <div>
+          <p>{profileData.nickname}</p>
+        </div>
+        
         <Link to="/users/edit/">
           <DynamicColorButton color="blue" text="프로필수정" />
         </Link>
@@ -62,7 +70,7 @@ function LeftMenu() {
       <div className='w-48 flex flex-col justify-start   h-56'>
         <div className='text-lg   font-semibold  self-start'>자기소개</div>
         <div className="self-start "> 
-          {userDataList.profile_content}
+          {profileData.profile_content}
         </div>
       </div>
       <Contour />
--- src/components/LeftMenu3.jsx
+++ src/components/LeftMenu3.jsx
@@ -1,68 +1,99 @@
-import React from 'react'
-import Margin from './Margin'
-import DynamicColorButton from './DynamicColorButton'
-import Contour from './ui/Contour'
-import { Link } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react';
+import Margin from './Margin';
+import DynamicColorButton from './DynamicColorButton';
+import Contour from './ui/Contour';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 function LeftMenu() {
+  // 사용되는 페이지가 없습니다.
+
+  const { getUserInfo } = useContext(AuthContext);
+  const [profileData, setProfileData] = useState({
+    profilePicture: '',
+    nickname: '',
+    profile_content: '',
+    profile_image:'',
+  });
+
+
+
+  useEffect(() => {
+    console.log("나는 레프트 매뉴 3이다")
+    const fetchData = async () => {
+      try {
+        const data = await getUserInfo();
+        setProfileData({
+          profile_image: data.profile_image || '',
+          nickname: data.nickname || '',
+          email: data.email || '',
+          profile_content: data.profile_content || "안녕하세요.",
+        });
+
+      } catch (error) {
+        console.error('Failed to fetch user profile:', error);
+      }
+    };
+
+    fetchData();
+  } ,[]);
+
   return (
     <div className='w-72 h-full flex justify-center items-center'>
-    <div className='mt-4 w-64 h-[500px] mx-4 border-[1px]  rounded-md bg-gray-100 flex flex-col items-center text-center'>
+      <div className='mt-4 w-64 h-[500px] mx-4 border-[1px]  rounded-md bg-gray-100 flex flex-col items-center text-center'>
 
-      {/* 상단 여백 적용 */}
-      <Margin top="5"   plustailwind="h-5" />
+        {/* 상단 여백 적용 */}
+        <Margin top="5" plustailwind="h-5" />
 
-      {/* 프로필 정보 */}
-      <div>
-        <div className='relative overflow-hidden rounded-full bg-black h-28 w-28'>
-          <img
-            src='프로필사진의_이미지_경로.jpg'
-            alt='프로필 사진'
-            className='프로필_이미지_스타일 rounded-full'
-          />
-          asdasd
+        {/* 프로필 정보 */}
+        <div>
+          <div className='relative overflow-hidden rounded-full bg-black h-28 w-28'>
+            <img
+              src={userInfo?.profile_image || '프로필사진의_이미지_경로.jpg'}
+              alt='프로필 사진'
+              className='프로필_이미지_스타일 rounded-full'
+            />
+          </div>
+          <Margin top="3" />
+
+          <Margin top="5" plustailwind="h-5" />
+          <Link to="/users/profile">
+            <DynamicColorButton color="blue" text="프로필수정" />
+          </Link>
+          <Margin top="5" plustailwind="h-5" />
         </div>
         <Margin top="3" />
-
-        <Margin top="5"   plustailwind="h-5" />
-        <Link to="/users/profile">
-        <DynamicColorButton color="blue" text="프로필수정" />
-        </Link>
-        <Margin top="5"   plustailwind="h-5" />
-      </div>
-      <Margin top="3" />
         {/* 수평 선 */}
         <Contour ></Contour>
-   
-      {/* 모집 정보 */}
-      <div className='w-48 flex flex-col justify-start   h-32'>
-        <div className='text-lg   font-semibold  self-start'>자기소개</div>
-        <div className="self-start "> 
-          자기 소개 api 받기
-        </div>
-      </div>
-      <Contour ></Contour>
 
-      <div className='w-48 flex flex-col justify-start'>
-        <div className='text-lg   font-semibold'>활동</div>
-        <div className="flex flex-col"> 
-          <div className='flex justify-between'>
-            <div className=''>받은 좋아요</div>
-            <div className=''>6명</div>
+        {/* 모집 정보 */}
+        <div className='w-48 flex flex-col justify-start h-32'>
+          <div className='text-lg font-semibold self-start'>자기소개</div>
+          <div className="self-start">
+            {userInfo?.content || '자기 소개 api 받기'}
           </div>
-          <div className='flex justify-between'>
-            <div  className=''>작성 게시글</div>
-            <div  className=''>4명</div>
-          </div>
-       
         </div>
-      </div>
+        <Contour ></Contour>
 
-      <Margin top="3" />
-   
+        <div className='w-48 flex flex-col justify-start'>
+          <div className='text-lg font-semibold'>활동</div>
+          <div className="flex flex-col">
+            <div className='flex justify-between'>
+              <div className=''>받은 좋아요</div>
+              <div className=''>{userInfo?.received_likes || '0'}명</div>
+            </div>
+            <div className='flex justify-between'>
+              <div className=''>작성 게시글</div>
+              <div className=''>{userInfo?.written_posts || '0'}명</div>
+            </div>
+          </div>
+        </div>
+
+        <Margin top="3" />
+
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default LeftMenu
+export default LeftMenu;
