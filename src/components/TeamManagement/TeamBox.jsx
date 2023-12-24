1fa42692a47b8f590486d15115e9522b9a9bbd33
sengwoong <92924243+sengwoong@users.noreply.github.com>
2023-12-24 15:37:53 +0900

    :sparkles: Feat: 팀관리 페이지 빠진 기능 추가

---
+++ src/components/TeamManagement/TeamBox.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Margin from "../Margin";
+import { Link } from "react-router-dom";
+
+function TeamBox({ teamData }) {
+  const { id, image, category, name } = teamData;
+  // 지금은 방법을 알 수가 없어서 임시 조치를 합니다.
+  // 기본 이미지들이 모델에 명시된 경로에 저장되지 않는 문제
+  // base url이 명시되지 않는 문제를 해결한 후 다시 수정하겠습니다.
+  const baseURL = import.meta.env.VITE_APP_API_KEY;
+
+  return (
+    <div className="flex">
+      <Link to={`/teams/${id}`} key={id}>
+        <div className="relative overflow-hidden rounded-full h-20 w-20 mr-2">
+          <img
+            src={baseURL+image}
+            alt="모임 사진"
+            className="모임_이미지_스타일 rounded-full"
+          />
+        </div>
+      </Link>
+      <Margin left="3" />
+      <Link to={`/teams/${id}`}>
+        <div className="bg-gray-100 w-96 rounded-lg h-20 pt-2 pl-2">
+          <p className="text-lg font-bold ml-1">{category}</p>
+          <p className="pt-1 ml-1">{name}</p>
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+export default TeamBox;
---
+++ src/components/TeamManagement/TeamBoxCrew.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Margin from '../Margin';
+import DynamicColorButton from '../DynamicColorButton';
+import crew from '../../assets/crew.png';
+import leader from '../../assets/leader.png';
+import { TeamLeave, applyToTeam, kickTeamMember } from '../../api/team';
+
+function TeamBoxCrew({ profileData,  postiId,isMe }) {
+  const { profilePicture,  is_leader, user,nickname } = profileData;
+
+
+
+  // 나가기버튼
+  const RefuseBtn = async () => {
+    try{
+    await TeamLeave({id:postiId})
+    window.location.reload();
+  
+    setPageReload((prev)=>{prev+1})}
+    catch{}
+  };
+  
+
+  // Define a dynamic class for the profile image style
+  const profileImageStyle = is_leader ? '프로필_이미지_스타일_leader' : '프로필_이미지_스타일_member';
+
+  return (
+    <div className={`flex ${is_leader ? 'bg-red-100' : 'bg-blue-100'}`}>
+      <div className={`relative overflow-hidden rounded-full ${is_leader ? 'bg-red-500' : 'bg-blue-500'} h-20 w-20`}>
+        <img
+          src={profilePicture ? profilePicture : is_leader ? leader : crew}
+          alt='프로필 사진'
+          className={profileImageStyle}
+        />
+      </div>
+      <Margin left='3' plustailwind='w-3' />
+      <div className={` w-72 rounded-lg h-20 flex justify-center items-center ${is_leader ? 'border-red-200 border-2' : 'border-blue-200 border-2'}`}>
+      {nickname}{is_leader ? '현재 팀장입니다' : '팀원 입니다'}
+      </div>
+      <Margin left='3' plustailwind='w-6' />
+      <div className='flex flex-col justify-center items-center'>
+  {user==isMe?(<DynamicColorButton btnstyle='w-24 h-8 mt-2' color='red' text='나가기' onClick={RefuseBtn}></DynamicColorButton>):(<></>)}
+
+      </div>
+    </div>
+  );
+}
+
+export default TeamBoxCrew;
