23969913f72a200926b97dd2b157aa9debd44137
sengwoong <92924243+sengwoong@users.noreply.github.com>
2023-12-25 15:48:11 +0900

    :bug: Fix:리엑트 쿼이 빌드버그 해결3

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { AuthContextProvider } from "./context/AuthContext";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { ModelContextProvider } from "./context/ModelContextProvider";
 import { BottomMenuProvider } from "./context/BottomMenu";
 import "./index.css";
 import { OpenModalProvider } from "./context/OpenModalProvider";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const queryClient = new QueryClient();
 
